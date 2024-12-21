8a5d602454e7b9b9f18d60d08964747c1a165948
Rohit Ahuja <rohit23ahuja@gmail.com>
2024-12-21 12:29:21 +0530

    call stored procedure

--- jdbc-postgres/src/main/java/jdbc/postgres/JdbcPostgresApplication.java
+++ jdbc-postgres/src/main/java/jdbc/postgres/JdbcPostgresApplication.java
@@ -1,7 +1,23 @@
 package jdbc.postgres;
 
-public class JdbcPostgresApplication {
-    public static void main(String[] args) {
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
 
+public class JdbcPostgresApplication {
+    public static void main(String[] args) throws SQLException {
+        String url = "jdbc:postgresql://localhost:5432/liquibasepg_db";
+        Properties properties = new Properties();
+        properties.setProperty("user", "postgres");
+        properties.setProperty("password", "password");
+        properties.setProperty("escapeSyntaxCallMode", "callIfNoReturn");
+
+        Connection connection = DriverManager.getConnection(url, properties);
+        CallableStatement statement = connection.prepareCall("{call commitproc( ? )}");
+        statement.setInt(1, 100);
+        statement.execute();
+        statement.close();
     }
 }
