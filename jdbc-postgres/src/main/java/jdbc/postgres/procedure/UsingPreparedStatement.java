1bcc9cac4f83911597628f8bfdf4dd1de5a727f7
Rohit Ahuja <rohit23ahuja@gmail.com>
2025-05-29 12:17:47 +0530

    procedure calling using prepared statement

---
+++ jdbc-postgres/src/main/java/jdbc/postgres/procedure/UsingPreparedStatement.java
@@ -0,0 +1,34 @@
+package jdbc.postgres.procedure;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.Properties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class UsingPreparedStatement {
+    private static final Logger _log = LoggerFactory.getLogger(UsingPreparedStatement.class);
+
+    public static void main(String[] args) throws SQLException, ClassNotFoundException {
+        String url = "jdbc:postgresql://localhost:5432/liquibasepg_db";
+        Properties properties = new Properties();
+        properties.setProperty("user", "postgres");
+        properties.setProperty("password", "password");
+
+        Class.forName("org.postgresql.Driver");
+        try(Connection connection = DriverManager.getConnection(url, properties);
+            PreparedStatement preparedStatement = connection.prepareStatement("CALL player_by_birth(?, ?, ?, ?, ?)")) {
+            preparedStatement.setString(1, "VK15");
+            preparedStatement.setString(2, "Virat");
+            preparedStatement.setString(3, "Kohli");
+            preparedStatement.setLong(4, Long.parseLong("1988"));
+            preparedStatement.setLong(5, Long.parseLong("2011"));
+            preparedStatement.executeUpdate();
+        }
+
+    }
+}
+
